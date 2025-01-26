ad4e651ab2cfc9dd84b833578080119dd27c2230
zoar2307 <zoar2307@gmail.com>
2025-01-26 11:49:00 +0200

    Fix some bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/(.*)", // Apply headers to all routes
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;",
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "SAMEORIGIN",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "no-referrer",
+                    },
+                    {
+                        key: "Permissions-Policy",
+                        value: "geolocation=(), microphone=(), camera=(), fullscreen=(self)",
+                    },
+                ],
+            },
+        ];
+    },
+};
 
 export default nextConfig;
